a42e095ab551c238c7d969b578f711b723d68e7e
RushabRisal <rushabofficials@gmail.com>
2024-06-08 16:25:39 +0545

    way to validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 
-import {react, useRef, useState} from 'react';
-import {FormInput} from './components/FormInput'
+import {react, useState} from 'react';
+import {FormInput} from './components/FormInput';
+import inputs from './OwnApi/Inputs.json'
 function App() {
-  const usernameref=useRef();
+  // const usernameref=useRef();
+  const [value,setValue]=useState({
+    username:'',
+    email :'',
+    age:'',
+    password:''
+  });
 
-  console.log(usernameref);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    // const data=new FormData(e.target);
+    // console.log(Object.fromEntries(data.entries()))
+    
+  }
+  const onChange=(e)=>{
+    setValue({...value,[e.target.name]:e.target.value})
+  }
+  console.log(value);
   return (
-    <div className='flex justify-center items-center p-4  h-dvh  '>
-      <form className=' rounded-xl opacity-67 bg-slate-300  md:w-96 container h-96 flex flex-col  shadow-2xl  p-5'>
+    <>
+    <div className='flex bg-img   justify-center items-center  h-dvh p-5 '>
+      <form onSubmit={handleSubmit} className=' rounded-xl backdrop-blur-sm  bg-white/55 border border-white   md:w-96 container flex flex-col  shadow-2xl  p-5'>
         <h1 className='text-center text-black font-bold text-xl'>Form</h1>
-          <FormInput placeholder={"Username"} refer={usernameref}/>
-          <FormInput placeholder={"Email"} refer={usernameref}/>
-          <FormInput placeholder={"Age"}/>
-          <FormInput placeholder={"Password"}/>
+          {
+            inputs.map((input)=>(
+              <FormInput key={input.id} {...input} value={value[input.name]} onChange={onChange}/>
+            ))
+          }
+          
+       
           <div className='flex justify-center items-center h-full '>
             <button className=' bg-slate-900 py-2 px-10 rounded-md   text-white'>Submit</button>
           </div>
       </form>
     </div>
+
+    </>
   );
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,11 @@
 import {React,useState} from 'react'
 
-export const FormInput = (props) => {
+export const FormInput = ({label,onChange,id,...inputProps}) => {
 
   return (
     <div className='m-3 border-solid border-2 flex flex-col justify-center align-center text-white '>
-        {/* <label htmlFor="useName">UserName</label> */}
-        <input type="text" id='useName' className='border-solid border-1 border-slate-900 p-2  bg-white ' ref={props.refer} placeholder={props.placeholder}/>
+        {/* <label htmlFor="useName">{label}</label> */}
+        <input   className='border-solid text-black border-1 border-slate-900 p-2  bg-white ' {...inputProps} onChange={onChange}/>
     </div>
   )
 }
